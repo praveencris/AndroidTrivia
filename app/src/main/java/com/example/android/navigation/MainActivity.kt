9cef3d96d5a32307729286fc2df71a275946631f
Praveen <pkchmits@gmail.com>
2020-09-28 15:12:29 +0530

    Drawer locked or unlocked based on the id of currentDestination Added addOnDestinationChangedListener

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -20,12 +20,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavGraph
-import androidx.navigation.Navigation
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.navigateUp
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -37,17 +36,27 @@ class MainActivity : AppCompatActivity() {
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        drawerLayout=binding.drawerLayout
+        drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController,drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
-        appBarConfiguration= AppBarConfiguration(navController.graph,drawerLayout)
-        NavigationUI.setupWithNavController(binding.navView,navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nv: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nv.graph.startDestination) {
+               drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+               drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
         //return NavigationUI.navigateUp(navController,appBarConfiguration)
-        return NavigationUI.navigateUp(navController,drawerLayout)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
