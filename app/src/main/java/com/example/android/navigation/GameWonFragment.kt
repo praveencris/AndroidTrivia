30a37fb6a5dc764075323cfd3f5ca9a35f87e4a6
Praveen <pkchmits@gmail.com>
2020-09-28 12:07:30 +0530

    Added arguments to GameWonFragment in navigation resource file Pass those arguments in action mentioned in GameFragment

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -27,6 +28,14 @@ import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
 class GameWonFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Fetch Arguments from GameWonFragmentArgs
+        val args=GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context,"Num Correct ${args.numCorrect} and Num Questions ${args.numQuestions}",Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
